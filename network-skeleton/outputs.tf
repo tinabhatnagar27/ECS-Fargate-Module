116ff77fe53bd3df3ad7335a0cbb6a9796488926
tinabhatnagar27 <154892884+tinabhatnagar27@users.noreply.github.com>
2026-07-06 15:02:58 +0530

    Add descriptions to output variables in outputs.tf

--- network-skeleton/outputs.tf
+++ network-skeleton/outputs.tf
@@ -1,31 +1,39 @@
 output "vpc_id" {
-  value = aws_vpc.this.id
+  description = "VPC ID."
+  value       = aws_vpc.this.id
 }
 
 output "public_subnet_ids" {
-  value = [for subnet in aws_subnet.public : subnet.id]
+  description = "Public subnet IDs."
+  value       = [for subnet in aws_subnet.public : subnet.id]
 }
 
 output "private_subnet_ids" {
-  value = [for subnet in aws_subnet.private : subnet.id]
+  description = "Private subnet IDs."
+  value       = [for subnet in aws_subnet.private : subnet.id]
 }
 
 output "alb_dns_name" {
-  value = aws_lb.this.dns_name
+  description = "Application Load Balancer DNS name."
+  value       = aws_lb.this.dns_name
 }
 
 output "alb_url" {
-  value = "http://${aws_lb.this.dns_name}"
+  description = "Application URL."
+  value       = "http://${aws_lb.this.dns_name}"
 }
 
 output "ecs_cluster_name" {
-  value = module.ecs_fargate.cluster_name
+  description = "ECS cluster name."
+  value       = module.ecs_fargate.cluster_name
 }
 
 output "ecs_service_name" {
-  value = module.ecs_fargate.service_name
+  description = "ECS service name."
+  value       = module.ecs_fargate.service_name
 }
 
 output "ecs_task_definition_arn" {
-  value = module.ecs_fargate.task_definition_arn
+  description = "ECS task definition ARN."
+  value       = module.ecs_fargate.task_definition_arn
 }
